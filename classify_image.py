2afd6a08d5168ace6a7c421dab24bcb66e18dd96
R Lewis <roblewis1237@gmail.com>
2018-08-15 17:44:46 +0100

    setting up the code to classify the image

--- classify_image.py
+++ classify_image.py
@@ -1,19 +1,14 @@
-from sklearn.datasets import load_files
 import numpy as np
-import pickle
-from tqdm import tqdm
+import tensorflow as tf
+import flask
+import io
 
 from keras.models import load_model
-from keras.utils import np_utils
-from keras.preprocessing import image
-from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.models import Sequential
+from keras.applications import imagenet_utils
+from keras.preprocessing.image import img_to_array
 
-from PIL import ImageFile
-ImageFile.LOAD_TRUNCATED_IMAGES = True
+from PIL import Image
 
-import flask
 
 # initialize our Flask application
 app = flask.Flask(__name__)
@@ -21,13 +16,70 @@ app = flask.Flask(__name__)
 
 def load_cnn_model():
     """ Loads the model using the keras hdf5 file saved to disk """
-    return load_model('saved_models/final_trained_model.h5')
+    global model
+    model = load_model('saved_models/final_trained_model.h5')
+
+    global graph  # Attempt to make keras run on threads
+    graph = tf.get_default_graph()
 
 
+def prepare_image(image, target):
+    """ Converts the image to the required format for classification """
+
+    # If the image mode is not RGB, convert it
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+
+    # resize the input image and preprocess it
+    image = image.resize(target)
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    image = imagenet_utils.preprocess_input(image)
+
+    # return the processed image
+    return image
+
+
+@app.route("/predict", methods=["POST"])
 def classify_image():
     """ Classifies an image provided by the user
      NB: this service is exposed as an endpoint """
-    pass
+
+    # Iinitialize the data dictionary that will be returned from the view
+    data = {"success": False}
+
+    # Ensure an image was properly uploaded to our endpoint
+    if flask.request.method == "POST":
+        if flask.request.files.get("image"):
+            # read the image in PIL format
+            image = flask.request.files["image"].read()
+            image = Image.open(io.BytesIO(image))
+
+            # Preprocess the image and prepare it for classification
+            image = prepare_image(image, target=(224, 224))
+
+            # classify the input image and then initialize the list
+            # of predictions to return to the client
+
+            # Code for threadsafing (required to make code run on AWS EC2)
+            global graph
+            with graph.as_default():
+                preds = model.predict(image)
+
+            # results = imagenet_utils.decode_predictions(preds)
+            data["predictions"] = preds
+
+            # loop over the results and add them to the list of
+            # returned predictions
+            # for (imagenetID, label, prob) in results[0]:
+            #     r = {"label": label, "probability": float(prob)}
+            #     data["predictions"].append(r)
+
+            # indicate that the request was a success
+            data["success"] = True
+
+    # return the data dictionary as a JSON response
+    return flask.jsonify(data)
 
 # if this is the main thread of execution first load the model and
 # then start the server
